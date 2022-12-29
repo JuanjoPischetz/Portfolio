77d8c1e28300f3b7e5450b20fa5e7e3faa2792c2
JuanjoPischetz <juanjo.pischetz@gmail.com>
2022-12-29 09:54:01 -0300

    mobile done

--- portfolio/src/components/contact/ContactUs.js
+++ portfolio/src/components/contact/ContactUs.js
@@ -47,23 +47,26 @@ const inputHandler = function(e){
 
   return (
     <div className='w-full rounded-lg shadow-xl shadow-[#2D2525] bg-[#AFFFCE] flex justify-center'>
-    <form ref={form} onSubmit={sendEmail} className='m-2 p-2'>
+    <form ref={form} onSubmit={sendEmail} className='m-2 p-2 '>
     <div className='flex flex-col w-[300px]'>
       <label className='text-[#7A1E33] font-bold text-lg'>Name</label>
       <input className='mb-2' type="text" name="user_name" placeholder='What is your name?'
       value={input.user_name} onChange={inputHandler}/>
-      <p>{errors.name}</p>
+      <p className='text-red-900'>{errors.name}</p>
       <label className='text-[#7A1E33] font-bold text-lg'>Email</label>
       <input className='mb-2' type="email" name="user_email" placeholder='Your Email'
       value={input.user_email} onChange={inputHandler}/>
-      <p>{errors.email}</p>
+      <p className='text-red-900'>{errors.email}</p>
       <label className='text-[#7A1E33] font-bold text-lg'>Message</label>
       <textarea className='mb-2' name="message" placeholder='How can I help you?' cols={1} rows={3}
       value={input.message} onChange={inputHandler} />
-      <p>{errors.message}</p>
+      <p className='text-red-900'>{errors.message}</p>
+      <div className='w-full flex flex-row justify-center mt-2'>
       <button type='submit' disabled={!(input.user_name !== '' && !errors.user_email && !errors.message)}
-      className={(input.user_name !== '' && !errors.user_email && !errors.message)?"bg-red border-4 p-4":" bg-blue border-2 p-4"}
+      className={!(input.user_name !== '' && !errors.user_email && !errors.message)?"bg-[#7A1E33] bg-opacity-[0.4] cursor-default text-lg text-gray-100 w-min px-4 py-2 rounded-xl"
+      :" bg-[#7A1E33] cursor-pointer text-lg text-gray-100 w-min px-4 py-2 rounded-xl"}
       >Send!</button>
+      </div>
     </div>
     </form>
     </div>
